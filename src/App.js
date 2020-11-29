8b958e47abc2a9aba38fce4d02ca42ede2fcccba
David Orson <onlyorson@outlook.com>
2020-11-29 00:07:24 +0000

    App and Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import './App.css';
+
+import Header from './components/Header';
+import Input from './components/Input';
+import Output from './components/Output';
+import Code from './components/Code';
+import Explanation from './components/Explanation';
 
 const App = () => {
+  const [string, setString] = useState('')
+  const [palindrone, setPalindrone] = useState(null);
+
+  const reverser = (string) => {
+    if (string) {
+      let reversed = (string.split('').reverse().join(''))
+
+      setPalindrone(((reversed === string).toString()));
+    } else {
+      setPalindrone('error')
+    }
+    
+
+
+    /* let reversed = '';
+
+    for (let character of string) {
+      reversed = character + reversed;
+    }
+
+    setReversedString(reversed) */
+
+    /* setReversedString(string.split('').reduce((rev, char) => char + rev, '')); */
+  }
+
   return (
-    <div>
-      hi
+    <div className="app-container">
+      <div className="app">
+        <Header />
+        <div className="app-main">
+          <Input string={string} setString={setString} reverser={reverser} />
+          <Output palindrone={palindrone} />
+          <p className="sign">App by David Orson</p>
+        </div>
+        <div className="app-secondary">
+          <Code />
+          <Explanation />
+        </div>
+      </div>
     </div>
   )
 }
