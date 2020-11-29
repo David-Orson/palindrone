6bbd4a9afdd477f9e97b304c6465537bbfeedc81
David Orson <onlyorson@outlook.com>
2020-11-29 00:07:58 +0000

    Inout

---
+++ src/components/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import "../css/Input.css"
+
+const Input = (props) => {
+  const {string, setString, reverser} = props
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    reverser(string)
+  } 
+
+  return (
+    <div className="input">
+      <form className="input-form" onSubmit={handleSubmit} >
+        <label className="input-label">Enter Your String!</label>
+        <input className="input-input" type="text" value={string} onChange={(e) => setString(e.target.value)}  />
+        <button className="input-button">TEST</button>
+      </form>
+    </div>
+  )
+}
+
+export default Input;
